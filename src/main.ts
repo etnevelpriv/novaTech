a382ec031b608d04e57a1967011636d2860539c8
etnevelpriv <leventerastovits@gmail.com>
2026-04-10 15:41:03 +0200

    faq animacio kb kesz

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const init = function () {
+    const faqElements = document.querySelectorAll('.faq-element');
+    faqElements.forEach((element) => {
+        const question = element.firstElementChild;
+        const answer = element.lastElementChild;
+        answer?.classList.add('closed')
+        question?.addEventListener("click", () => {
+            answer?.classList.toggle('closed');
+        });
+    });
+};
+
+document.addEventListener("DOMContentLoaded", init)
